4cab4b8a6e0a4889153f435aaaba346868e65ee6
Samay2204 <shauryanamdeo2204@gmail.com>
2025-07-27 18:59:35 +0530

    added ws schema

--- webSockets/src/index.ts
+++ webSockets/src/index.ts
@@ -3,7 +3,7 @@ import { WebSocketServer,WebSocket } from "ws";
 const wss = new WebSocketServer({port: 8080});
 
 let userCount = 0;
-let allSocket: WebSocket[] = [];
+let allSocket: User[] = [];
 
 
 interface User{
@@ -26,10 +26,31 @@ wss.on('connection', function ws(socket){
 
    socket.on('message', function message(data){
       console.log("message recieved" + data.toString());
+      //@ts-ignore
+      const parsedMessage = JSON.parse(data);
 
-      for(let i=0; i<allSocket.length; i++){
-         allSocket[i].send(data.toString() + "sent from server to all clients");
+      if(parsedMessage.type === "join"){
+             allSocket.push({
+               socket: socket,
+               room: parsedMessage.payload.roomid
+             })
       }
+
+
+      if(parsedMessage.type === "chat"){
+           let currentUser = null;
+
+           for(let i=0; i<allSocket.length(); i++ ){
+              
+           }
+      }
+      
+
+      
+      
+      // for(let i=0; i<allSocket.length; i++){
+      //    allSocket[i].send(data.toString() + "sent from server to all clients");
+      // }
    });
 
    // if people leave the ws server, need to remove them from allsocket array
